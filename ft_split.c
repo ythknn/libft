9d2094423d57907dff353d1b4022c467d533874c
tarik <66226638+ythknn@users.noreply.github.com>
2024-11-06 14:14:18 +0300

    free

--- ft_split.c
+++ ft_split.c
@@ -12,49 +12,57 @@
 
 #include "libft.h"
 
-static size_t	ft_countword(char const *s, char c)
+int	ft_countword(char const *s, char c)
 {
-	size_t	count;
+	int	a;
 
-	if (!*s)
-		return (0);
-	count = 0;
+	a = 0;
 	while (*s)
 	{
 		while (*s == c)
 			s++;
 		if (*s)
-			count++;
-		while (*s != c && *s)
+			a++;
+		while (*s && *s != c)
 			s++;
 	}
-	return (count);
+	return (a);
+}
+
+static void	ft_dispose_arr(char **arr, int i)
+{
+	while (i >= 0)
+	{
+		free(arr[i]);
+		i--;
+	}
+	free(arr);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**lst;
-	size_t	word_len;
+	char	**final;
 	int		i;
+	int		j;
+	int		len;
 
-	lst = (char **)malloc((ft_countword(s, c) + 1) * sizeof(char *));
-	if (!s || !lst)
-		return (0);
-	i = 0;
-	while (*s)
+	len = ft_countword(s, c);
+	final = malloc(sizeof(char *) * (len + 1));
+	if (!final)
+		return (NULL);
+	i = -1;
+	while (++i < len)
 	{
-		while (*s == c && *s)
+		while (*s == c)
 			s++;
-		if (*s)
-		{
-			if (!ft_strchr(s, c))
-				word_len = ft_strlen(s);
-			else
-				word_len = ft_strchr(s, c) - s;
-			lst[i++] = ft_substr(s, 0, word_len);
-			s += word_len;
-		}
+		j = 0;
+		while (s[j] != c && s[j])
+			j++;
+		final[i] = ft_substr(s, 0, j);
+		if (!final[i])
+			return (ft_dispose_arr(final, i), NULL);
+		s += j;
 	}
-	lst[i] = NULL;
-	return (lst);
+	final[i] = NULL;
+	return (final);
 }
